11ab6ae88b6e89d8c23e062ce3f295fc22b5ee2c
Sergey-whiteserg88 <whiteserg88@yandex.ru>
2022-10-12 23:13:31 +0300

    Сохранение1

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Client {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+class Client implements Runnable {
+    Socket socket;
+    Scanner in;
+    PrintStream out;
+    Main main;
+
+    public Client(Socket socket, Main main){
+
+        this.socket = socket;
+        this.main = main;
+        new Thread(this).start();
+    }
+    void receive(String msg){
+        out.println(msg);
+
+    }
+
+    public void run() {
+        try {
+            // получаем потоки ввода и вывода
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            // создаем удобные средства ввода и вывода
+            in = new Scanner(is);
+            out = new PrintStream(os);
+
+            // читаем из сети и пишем в сеть
+            out.println("Welcome to chat!");
+            String input = in.nextLine();
+            while (!input.equals("bye")) {
+                main.send(input);
+                input = in.nextLine();
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import javax.swing.plaf.TableHeaderUI;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    ArrayList<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+    Main() throws IOException {
+        serverSocket = new ServerSocket(1234);
+    }
+    void  send(String msg){
+        for (Client client:clients){
+            client.receive(msg);
+        }
+    }
+    public void run(){
+        while (true){
+            System.out.println("Waiting...");
+            try {
+                Socket socket = serverSocket.accept();
+            System.out.println("Client connected!");
+            clients.add(new Client(socket, this));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        new Main().run();
+
+
     }
 }
